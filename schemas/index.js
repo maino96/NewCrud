667033bba6761b71ab87c2defb0cc67dbf983166
minho park <kingpmh96@gamil.com>
2022-10-06 20:19:46 +0900

    update

--- schemas/index.js
+++ schemas/index.js
@@ -1,23 +1,19 @@
+const dotenv = require('dotenv'); 
 const mongoose = require("mongoose");
-
-// const connect = () => {
-//   mongoose
-//     .connect("mongodb://localhost:27017/homework", {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       ignoreUndefined: true,
-//     })
-//     .catch(err => console.log(err));
-// };
-
-// mongoose.connection.on("error", err => {
-//   console.error("몽고디비 연결 에러", err);
-// });
+dotenv.config();
 
 const connect = () => {
-  mongoose.connect(process.env.DB_URL, { ignoreUndefined: true }).catch((err) => {
-  console.error(err);
-  });
-  };
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ignoreUndefined: true,
+    })
+    .catch(err => console.log(err));
+};
+
+mongoose.connection.on("error", err => {
+  console.error("몽고디비 연결 에러", err);
+});
 
 module.exports = connect;
